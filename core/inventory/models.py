97407fe18739761389f5462ac5f40e819f00292a
arcanovarelav22-creator <arcano.varelav22@gmail.com>
2026-06-15 16:33:26 -0400

    Configure models.ImageField and media settings for product images-2

--- core/inventory/models.py
+++ core/inventory/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price_per_unit = models.DecimalField(max_length=10, decimal_places=2)
+    # 🎯 CORREGIDO: max_digits en lugar de max_length
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
     is_critical = models.BooleanField(default=False)
-    
-    # 🎯 CAMBIA O AGREGA ESTA LÍNEA:
-    # upload_to indica la carpeta interna o el prefijo donde se organizarán las fotos
     image_url = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
